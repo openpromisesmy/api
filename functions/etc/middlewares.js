5b65cf2183392ec379313f3dde7cedef9ea7290f
Nazreen <nxmohamad@gmail.com>
2018-03-15 21:55:14 +0800

    check for firebase Token

--- functions/etc/middlewares.js
+++ functions/etc/middlewares.js
@@ -1,8 +1,22 @@
+const admin = require('firebase-admin');
+
 const firebaseAuth = function(req, res, next) {
-  if (!req.headers['X_FIREBASE_TOKEN']) {
+  const sentToken = req.headers['x_firebase_token'];
+  if (!sentToken) {
     res.status(400);
     res.send('None shall pass');
   } else {
+    admin
+      .auth()
+      .verifyIdToken(sentToken)
+      .then(function(decodedToken) {
+        var uid = decodedToken.uid;
+      })
+      .catch(function(error) {
+        res.status(400);
+        res.send('None shall pass');
+      });
+
     next();
   }
 };
