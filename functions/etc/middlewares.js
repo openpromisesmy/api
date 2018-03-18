73f6cb9d4cf545831d89115fa8eb7ee65c117714
Nazreen <nxmohamad@gmail.com>
2018-03-18 22:21:41 +0800

    mark TODOs in firebaseAuth middleware

--- functions/etc/middlewares.js
+++ functions/etc/middlewares.js
@@ -2,6 +2,10 @@ const admin = require('firebase-admin');
 
 const firebaseAuth = function(req, res, next) {
   const sentToken = req.headers['x_firebase_token'];
+  const userEmail = req.headers['x_user_email'];
+  const userName = req.headers['x_user_name'];
+  const userPhoto = req.headers['x_user_photo'];
+
   if (!sentToken) {
     res.status(400);
     res.send('None shall pass');
@@ -10,6 +14,12 @@ const firebaseAuth = function(req, res, next) {
       .auth()
       .verifyIdToken(sentToken)
       .then(decodedToken => {
+        // TODO: check whether userEmail already in list of contributors
+
+        // TODO: if exists, get the contributor_id
+        // TODO: if does not exist, create contributor then get contributor_id
+        // TODO: attach contributor_id in request.params.contributor_id
+
         var uid = decodedToken.uid;
         return uid;
       })
