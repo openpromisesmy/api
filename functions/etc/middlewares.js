c5799b5811f94f8668b6809a6a0eb8539e296ed7
Nazreen <nxmohamad@gmail.com>
2018-06-09 04:37:17 +0800

    error 403 if expired token (#26)
        previously it was 400 which does not indicate the problem
        this change allows clients to react accordingly

--- functions/etc/middlewares.js
+++ functions/etc/middlewares.js
@@ -88,7 +88,7 @@ const firebaseAuth = function(req, res, next) {
       })
       .catch(error => {
         console.error(error);
-        res.status(400);
+        res.status(403);
         res.send('There has been an error in authorization.');
       });
   }
