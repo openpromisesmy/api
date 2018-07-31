d000bbe0a817b79fa8681f017107c70842c6f035
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-31 20:32:32 +0800

    register function

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,13 @@ admin.initializeApp(functions.config().firebase);
 const contributors = require('./api/contributors');
 const politicians = require('./api/politicians');
 const promises = require('./api/promises');
+const promiseUpdates = require('./api/promiseUpdate');
 const stats = require('./api/stats');
 
 module.exports = {
   contributors,
   politicians,
   promises,
+  promiseUpdates,
   stats
 };
