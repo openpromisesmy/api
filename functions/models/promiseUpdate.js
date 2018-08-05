21055d313a4b82dc04ce555ce334064a530690cc
Nazreen Mohamad <nxmohamad@gmail.com>
2018-08-05 23:42:40 +0800

    update promise.status based on latest status in array

--- functions/models/promiseUpdate.js
+++ functions/models/promiseUpdate.js
@@ -65,17 +65,31 @@ const list = () =>
       .catch(e => reject(e));
   });
 
-const update = (id, updateData) =>
+const update = (id, validatedData) =>
   new Promise((resolve, reject) =>
     get(id)
       .then(promiseUpdate => {
         if (_.isEmpty(promiseUpdate))
           return resolve({ status: 404, message: 'Invalid Promise Update' });
 
+        // TODO: check if promise exists, like in add
+
+        // update status of Promise based on latest status of latest update
+        // TODO: do we need to force sort the updates array based on source date?
+        if (validatedData.updates.length > 0) {
+          const latestStatus =
+            validatedData.updates[validatedData.updates.length - 1].status;
+          console.log(latestStatus);
+          promise
+            .update(validatedData.promise_id, { status: latestStatus })
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
+        }
+
         // @TODO: fix https://github.com/xjamundx/eslint-plugin-promise/issues/42
         return collection
           .doc(id)
-          .update(updateData)
+          .update(validatedData)
           .then(d => resolve(d))
           .catch(e => reject(id));
       })
