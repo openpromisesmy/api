ced9200223590a8546bbaca4142de69397196de4
Nazreen <nxmohamad@gmail.com>
2018-08-10 16:57:01 +0800

    feat(promiseUpdate): setup (#48)
    * update schema
    
    * model
    
    * api
    
    * register function
    
    * typo
    
    * typo
    
    * min 1
    
    * min 1
    
    * require created_at
    
    * use string().isoDate()
    
    * make status required()
    
    * update promise.status based on latest status in array
    
    * remove console.log
    
    * update schema so that one promiseUpdate hosts one update
    
    * add query to list promiseUpdates
    
    * make status required
    
    * create updateHostPromiseStatus method
    
    * rename variable
    
    * remove console.log
    
    * resolve only after updateHostPromiseStatus is resolved
    
    * rename method

---
+++ functions/models/promiseUpdate.js
@@ -0,0 +1,156 @@
+const admin = require('firebase-admin');
+const db = admin.firestore();
+const _ = require('lodash');
+const util = require('../etc/util');
+const promiseModel = require('./promise');
+const promise = promiseModel();
+const schema = require('../schemas/promiseUpdate');
+const createSchema = schema.create;
+const updateSchema = schema.update;
+
+const collection = db.collection('promiseUpdates');
+
+const add = data =>
+  new Promise((resolve, reject) =>
+    promise
+      .get(data.promise_id)
+      .then(promise => {
+        if (_.isEmpty(promise))
+          return resolve({ status: 404, message: 'Invalid Promise' });
+
+        // @TODO: fix https://github.com/xjamundx/eslint-plugin-promise/issues/42
+        return collection
+          .add(data)
+          .then(ref => {
+            if (_.isEmpty(ref)) return reject(new Error('Failed to add'));
+
+            return updateSourcePromiseStatus(data.promise_id)
+              .then(res => resolve({ id: ref.id }))
+              .catch(err => console.err(err));
+          })
+          .catch(e => {
+            console.error(e);
+            return reject(e);
+          });
+      })
+      .catch(e => {
+        if (e.status) return resolve(e);
+
+        console.error(e);
+        return reject(e);
+      })
+  );
+
+const get = id =>
+  new Promise((resolve, reject) =>
+    collection
+      .doc(id)
+      .get()
+      .then(doc => {
+        const data = doc.data();
+        const result = _.isEmpty(data) ? {} : util.toObject(id, data);
+
+        return resolve(result);
+      })
+      .catch(e => {
+        console.log(e);
+        return reject(e);
+      })
+  );
+
+// add in query for promise_id, and source_date (asc)
+const list = query =>
+  new Promise((resolve, reject) => {
+    const paginationQueries = ['orderBy', 'reverse'];
+    let ref = collection;
+    if (!_.isEmpty(query)) {
+      for (let x in query) {
+        if (paginationQueries.includes(x)) {
+          // for pagination
+          switch (x) {
+            case 'orderBy':
+              ref = ref.orderBy(query[x], query.reverse ? 'desc' : 'asc');
+              break;
+            default:
+              break;
+          }
+        } else {
+          // for other queries
+          ref = ref.where(x, '==', query[x]);
+        }
+      }
+    }
+    ref
+      .get()
+      .then(snapshot => {
+        return resolve(util.snapshotToArray(snapshot));
+      })
+      .catch(e => reject(e));
+  });
+
+const update = (id, data) =>
+  new Promise((resolve, reject) =>
+    get(id)
+      .then(promiseUpdate => {
+        if (_.isEmpty(promiseUpdate))
+          return resolve({ status: 404, message: 'Invalid Promise Update' });
+
+        // @TODO: fix https://github.com/xjamundx/eslint-plugin-promise/issues/42
+        return collection
+          .doc(id)
+          .update(data)
+          .then(d => {
+            return updateSourcePromiseStatus(data.promise_id)
+              .then(res => resolve(d))
+              .catch(err => console.err(err));
+          })
+          .catch(e => reject(id));
+      })
+      .catch(e => {
+        console.error(e);
+        return reject(e);
+      })
+  );
+
+const remove = id =>
+  new Promise((resolve, reject) =>
+    collection
+      .doc(id)
+      .delete()
+      .then(() => resolve())
+      .catch(e => {
+        console.error(e);
+        return reject(e);
+      })
+  );
+
+const updateSourcePromiseStatus = promiseID =>
+  new Promise((resolve, reject) =>
+    list({ promise_id: promiseID, orderBy: 'source_date' })
+      .then(promiseUpdates => {
+        const latestUpdate = promiseUpdates[promiseUpdates.length - 1];
+        const latestStatus = latestUpdate.status;
+
+        // TODO: avoid update if no change
+        promise
+          .update(promiseID, { status: latestStatus })
+          .then(res => console.log(res))
+          .catch(err => console.error(err));
+      })
+      .catch(e => {
+        console.error(e);
+        return reject(e);
+      })
+  );
+
+const promiseUpdate = () => ({
+  createSchema,
+  updateSchema,
+  list,
+  get,
+  add,
+  update,
+  remove
+});
+
+module.exports = promiseUpdate;
