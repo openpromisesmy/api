1c1f4f96e9ac32efc52c114694cedf6d46e4c1fd
Nazreen <nxmohamad@gmail.com>
2019-01-14 03:15:51 +0800

    feat: setup PromiseLead interface (#63)
    * create PromiseLead schema
    
    * make contributor email optional

---
+++ functions/schemas/promiseLead.ts
@@ -0,0 +1,8 @@
+export interface IPromiseLead {
+  contains_promise: boolean;
+  contributor_email?: string;
+  notes?: string;
+  processed: boolean;
+  source_url: string;
+  tracker_id?: string;
+}
