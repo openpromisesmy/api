6bcbcecee4f036e8f4cf2a7e03755f7903d4593a
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-14 03:14:56 +0800

    make contributor email optional

--- functions/schemas/promiseLead.ts
+++ functions/schemas/promiseLead.ts
@@ -1,6 +1,6 @@
 export interface IPromiseLead {
   contains_promise: boolean;
-  contributor_email: string;
+  contributor_email?: string;
   notes?: string;
   processed: boolean;
   source_url: string;
