07646d10f3c45cb1278ea921d1666486bc253098
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-03-08 22:11:21 +0200

    Set up the Mocha testing framework
    Add a sample test

---
+++ functions/test/sample.test.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import { expect } from 'chai';
+
+describe('calculate', function() {
+  it('add', function() {
+    const result = 5 + 2;
+    expect(result).to.equal(7);
+  });
+});
