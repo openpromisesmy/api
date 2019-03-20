6e815d8149079317e2587cb50f76a1d8895950a9
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-03-20 19:33:12 +0200

    feat: create list endpoints (stubs) (#71)
    * feat: set up list endpoint stubs
    
    * chore: move the 'notImplemented' function to the middlewares module
    
    * chore: move 'notImplemented' back to api/lists

---
+++ functions/api/lists.ts
@@ -0,0 +1,33 @@
+import cors from 'cors';
+import express from 'express';
+import * as functions from 'firebase-functions';
+
+import boolParser from 'express-query-boolean';
+import middlewares from '../etc/middlewares';
+
+const { firebaseAuth } = middlewares;
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.post('/', firebaseAuth, notImplemented);
+
+app.get('/all', firebaseAuth, notImplemented);
+
+app.get('/:id', firebaseAuth, notImplemented);
+
+app.post('/:id', firebaseAuth, notImplemented);
+
+app.delete('/:id', firebaseAuth, notImplemented);
+
+export = functions.https.onRequest(app);
+
+function notImplemented(
+  req: express.Request,
+  res: express.Response,
+  next?: Function
+) {
+  res.sendStatus(501);
+}
