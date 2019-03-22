87a0964fc647bc643976dabd41c5d83bb51b31cc
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-03-22 15:29:27 +0200

    feat: implement getList endpoint

--- functions/api/lists.ts
+++ functions/api/lists.ts
@@ -5,6 +5,8 @@ import * as functions from 'firebase-functions';
 import boolParser from 'express-query-boolean';
 import middlewares from '../etc/middlewares';
 
+import getList from './lists/get';
+
 const { firebaseAuth } = middlewares;
 
 const app = express();
@@ -16,7 +18,7 @@ app.post('/', firebaseAuth, notImplemented);
 
 app.get('/all', firebaseAuth, notImplemented);
 
-app.get('/:id', firebaseAuth, notImplemented);
+app.get('/:id', firebaseAuth, getList);
 
 app.post('/:id', firebaseAuth, notImplemented);
 
