8f9c575fb3816b522c2482db06c3662eda38d983
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-03-22 16:35:56 +0200

    feat: implement List#get (#75)

---
+++ functions/test/models/list/get.test.ts
@@ -0,0 +1,34 @@
+/// <reference path="./types.d.ts" />
+import FirebaseMock from 'mock-cloud-firestore';
+
+import 'mocha';
+import { expect } from 'chai';
+import { IList } from '../../../schemas/list';
+import get from '../../../models/list/get';
+
+describe('the get method of the List model', () => {
+  const record: IList = {
+    title: 'public safety',
+    promise_ids: ['abc1', 'abc2', 'abc3'],
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString()
+  };
+
+  let db: any;
+  let recordRef: any;
+
+  beforeEach(async () => {
+    db = new FirebaseMock().firestore();
+    recordRef = await db.collection('lists').add(record);
+  });
+
+  it('gets the List doc with the given id from the db', async () => {
+    const result = await get(recordRef.id, db);
+    expect(result).to.deep.equal(record);
+  });
+
+  it('returns undefined if no doc with the given id exists', async () => {
+    const result = await get('123', db);
+    expect(result).to.be.undefined;
+  });
+});
