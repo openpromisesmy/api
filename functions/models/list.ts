bf2abd418889467a84fdb69c3ce52838a79b83ae
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-22 23:31:08 +0800

    include create

--- functions/models/list.ts
+++ functions/models/list.ts
@@ -1,9 +1,11 @@
 import add from './list/add';
+import create from './list/create';
 import get from './list/get';
 import list from './list/list';
 
 export = () => ({
   add,
+  create,
   get,
   list
 });
