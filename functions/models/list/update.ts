8e90d843421191420ab04e120f5d18a2d2e8c6ec
Nazreen <nxmohamad@gmail.com>
2019-03-28 00:47:28 +0800

    feat(models/List): add update (#88)
    * add update
    
    * remove dupe
    
    * move to list/index

---
+++ functions/models/list/update.ts
@@ -0,0 +1,19 @@
+import admin from 'firebase-admin';
+import { IList } from '../../schemas/list';
+
+// TODO: specify return Type
+async function update(
+  id: string,
+  updateData: IList,
+  dbOverride?: admin.firestore.Firestore
+) {
+  const db = dbOverride || admin.firestore();
+  const result = await db
+    .collection('lists')
+    .doc(id)
+    .update(updateData);
+
+  return result;
+}
+
+export default update;
