128d448e5a87643296c4edcb54427d3654aaf513
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-03-29 00:11:06 +0200

    feat: implement support for queries in List.list

--- functions/models/list/list.ts
+++ functions/models/list/list.ts
@@ -5,13 +5,32 @@ import util = require('../../etc/util');
 import { IList } from '../../schemas/list';
 
 async function list(
-  query: object,
+  query: any,
   dbOverride?: admin.firestore.Firestore
 ): Promise<object[]> {
   const db = dbOverride || admin.firestore();
-  const snapshot = await db.collection('lists').get();
+  const snapshot = await filter(query, db).then(ref => ref.get());
 
   return util.snapshotToArray(snapshot);
+}
+
+async function filter(
+  query: any,
+  db: admin.firestore.Firestore
+): Promise<admin.firestore.Query> {
+  let queryChain: admin.firestore.Query = db.collection('lists');
+
+  for (const prop in query) {
+    switch (prop) {
+      case 'live':
+        queryChain = queryChain.where(prop, '==', query[prop]);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return queryChain;
 }
 
 export default list;
