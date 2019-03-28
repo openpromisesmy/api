7455ccd1ab52118e87db184bb6902c87afdf2b43
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-29 00:47:57 +0800

    rename vars

--- functions/models/promise/update.ts
+++ functions/models/promise/update.ts
@@ -14,12 +14,12 @@ const update = db => async (id: string, data: IPromise) => {
   if (_.isEmpty(promise)) {
     return { status: 404, message: 'Invalid Promise' };
   }
-  const previouslyNoListIds = !promise.list_ids || promise.list_ids.length < 1;
-  const previouslyHasListIds = !previouslyNoListIds;
-  const updateHasListIds = data.list_ids && data.list_ids.length > 0;
-  const updateDoesNotHaveListIds = !updateHasListIds;
+  const previouslyNone = !promise.list_ids || promise.list_ids.length < 1;
+  const previouslyHas = !previouslyNone;
+  const updateHas = data.list_ids && data.list_ids.length > 0;
+  const updateNone = !updateHas;
 
-  if (previouslyNoListIds && updateDoesNotHaveListIds) {
+  if (previouslyNone && updateNone) {
     return await db
       .collection('promises')
       .doc(id)
@@ -27,19 +27,21 @@ const update = db => async (id: string, data: IPromise) => {
   } else {
     return db
       .runTransaction(async (transaction: any) => {
-        if (updateHasListIds) {
+        if (updateHas) {
           await ensureAllListsExistById(data.list_ids);
         }
-        if (previouslyNoListIds && updateHasListIds) {
+
+        const change = detectArrayChanges(promise.list_ids, data.list_ids);
+        console.log(change);
+
+        if (previouslyNone && updateHas) {
           console.log('just update Lists with this promise_id');
           await findAllListsByIdAndAddPromiseId(data.list_ids, id, transaction);
         }
-        if (previouslyHasListIds && updateDoesNotHaveListIds) {
+        if (previouslyHas && updateNone) {
           console.log('remove promise_id from all List');
         }
-        if (previouslyHasListIds && updateHasListIds) {
-          const change = detectArrayChanges(promise.list_ids, data.list_ids);
-          console.log(change);
+        if (previouslyHas && updateHas) {
         }
         return collection.doc(id).update(data);
       })
