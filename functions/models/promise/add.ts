77af5d57c4596cfbb0053ebc9981a7718d8f5f7a
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-29 00:22:07 +0800

    make work

--- functions/models/promise/add.ts
+++ functions/models/promise/add.ts
@@ -7,23 +7,30 @@ import {
 } from './index';
 
 const add = db => async (data: IPromise) => {
-  console.log(data);
-  return db
-    .runTransaction(async (transaction: any) => {
-      await ensurePoliticianExistsById(data.politician_id);
-      await ensureContributorExistsById(data.contributor_id);
-      const ref = db.collection('promises').doc();
-      transaction.update(ref, data);
-      await ensureAllListsExistById(data.list_ids);
-      await findAllListsByIdAndAddPromiseId(data.list_ids, ref.id, transaction);
-      return { id: ref.id };
-    })
-    .catch((e: any) => {
-      if (e.status) {
-        return e;
-      }
-      throw e;
-    });
+  await ensurePoliticianExistsById(data.politician_id);
+  await ensureContributorExistsById(data.contributor_id);
+  const addResult = await db.collection('promises').add(data);
+
+  if (data.list_ids && data.list_ids.length > 0) {
+    return db
+      .runTransaction(async (transaction: any) => {
+        await ensureAllListsExistById(data.list_ids);
+        await findAllListsByIdAndAddPromiseId(
+          data.list_ids,
+          addResult.id,
+          transaction
+        );
+        return { id: addResult.id };
+      })
+      .catch((e: any) => {
+        if (e.status) {
+          return e;
+        }
+        throw e;
+      });
+  } else {
+    return { id: addResult.id };
+  }
 };
 
 export default add;
