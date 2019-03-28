e19cad1401bd3803b49d6aa1295773cef81532a6
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-29 01:40:00 +0800

    set default

--- functions/etc/utils/detectArrayChanges.ts
+++ functions/etc/utils/detectArrayChanges.ts
@@ -1,6 +1,6 @@
 // returns an object with the values as key_names and an integer value that represents the operation
 // 0 = no change, -1 = removed, 1 added
-function detectArrayChanges(originalArr, updatedArr) {
+function detectArrayChanges(originalArr = [], updatedArr) {
   const store = {};
   originalArr.forEach(item => {
     store[item] = -1;
