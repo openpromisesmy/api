8bca53dc6d159891d52b7963c620e4a8ffe0e560
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-29 21:16:22 +0800

    create constants

--- functions/etc/utils/detectArrayChanges.ts
+++ functions/etc/utils/detectArrayChanges.ts
@@ -1,5 +1,6 @@
-// returns an object with the values as key_names and an integer value that represents the operation
-// 0 = no change, -1 = removed, 1 added
+const ITEM_ADDED = 1;
+const ITEM_REMOVED = -1;
+
 function detectArrayChanges(originalArr = [], updatedArr) {
   const store = {};
   originalArr.forEach(item => {
@@ -14,11 +15,11 @@ function detectArrayChanges(originalArr = [], updatedArr) {
   });
 
   const additions = Object.keys(store).filter(key => {
-    return store[key] === 1;
+    return store[key] === ITEM_ADDED;
   });
 
   const removals = Object.keys(store).filter(key => {
-    return store[key] === -1;
+    return store[key] === ITEM_REMOVED;
   });
 
   return { additions, removals };
