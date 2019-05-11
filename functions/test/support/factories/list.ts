3f8dece05d20b47db9f80719415709f3d8c52e17
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-05-12 01:52:32 +0300

    fix: hardcode dates in the list factory

--- functions/test/support/factories/list.ts
+++ functions/test/support/factories/list.ts
@@ -6,8 +6,8 @@ const factory = Factory.Sync.makeFactory<IList>({
   live: true,
   promise_ids: ['abc1', 'abc2', 'abc3'],
   contributor_id: '123',
-  created_at: new Date().toISOString(),
-  updated_at: new Date().toISOString()
+  created_at: '2019-05-11T22:51:25.284Z',
+  updated_at: '2019-05-11T22:51:25.284Z'
 });
 
 export default factory;
