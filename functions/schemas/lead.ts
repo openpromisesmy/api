c4b6ad5bb83e7269edd22871f0e6e27b8fb9b56c
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-05-20 16:03:26 +0300

    feat: implement the ILead interface

--- functions/schemas/lead.ts
+++ functions/schemas/lead.ts
@@ -1,12 +1,56 @@
 import joi from 'joi';
+import utils from '../etc/utils';
 
-export const create = joi.object().keys({
-  link: joi.string().required(),
-  review_status: joi.string().required(),
-  type: joi.string().required(),
-  submitter: joi.string().required(),
-  reviewed_by: joi.string().required(),
-  notes: joi.string().required(),
+interface ILead {
+  assigned_tracker: string;
+  created_at: string;
+  link: string;
+  notes: string;
+  original_promise: string;
+  reviewed_by: string;
+  review_status: string;
+  submitter: string;
+  type: string;
+  updated_at: string;
+}
+
+const create = joi.object().keys({
   assigned_tracker: joi.string().required(),
-  original_promise: joi.string().required()
+  created_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of creation'),
+  link: joi.string().required(),
+  notes: joi.string().required(),
+  original_promise: joi.string().required(),
+  reviewed_by: joi.string().required(),
+  review_status: joi.string().required(),
+  submitter: joi.string().required(),
+  type: joi.string().required(),
+  updated_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of update')
 });
+
+const update = joi.object().keys({
+  assigned_tracker: joi.string().required(),
+  created_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of creation'),
+  link: joi.string().required(),
+  notes: joi.string().required(),
+  original_promise: joi.string().required(),
+  reviewed_by: joi.string().required(),
+  review_status: joi.string().required(),
+  submitter: joi.string().required(),
+  type: joi.string().required(),
+  updated_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of update')
+});
+
+export const create;
+export const update;
