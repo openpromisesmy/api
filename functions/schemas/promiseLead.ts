e341106d4c6ca0c405c08e280b22aa5cfd39887f
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-05-26 15:10:49 +0300

    chore: remove the deprecated promiseLead schema

--- functions/schemas/promiseLead.ts
+++
@@ -1,8 +0,0 @@
-export interface IPromiseLead {
-  contains_promise: boolean;
-  contributor_email?: string;
-  notes?: string;
-  processed: boolean;
-  source_url: string;
-  tracker_id?: string;
-}
