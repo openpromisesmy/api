42e52698a32997d4084d8dc667b858d28af48a56
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2019-06-02 21:46:38 +0300

    feat: implement lead schema (#108)
    * feat: implement the lead creation schema
    
    * feat: implement the ILead interface
    
    * chore: test the lead create schema
    
    * chore: test the lead update schema
    
    * feat: add 'id' to the ILead interface
    
    * feat: add the id field to the update schema
    
    * chore: remove the redundant TODO notes
    
    * feat: require the type field to be a supported value
    
    * fix: missing semicolon
    
    * chore: remove the deprecated promiseLead schema
    
    * fix: remove focus from the lead spec

---
+++ functions/schemas/lead.ts
@@ -0,0 +1,61 @@
+import joi from 'joi';
+import utils from '../etc/utils';
+
+export interface ILead {
+  assigned_tracker: string;
+  created_at: string;
+  id: string;
+  link: string;
+  notes: string;
+  original_promise: string;
+  reviewed_by: string;
+  review_status: string;
+  submitter: string;
+  type: string;
+  updated_at: string;
+}
+
+export const create = joi.object().keys({
+  assigned_tracker: joi.string().required(),
+  created_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of creation'),
+  link: joi.string().required(),
+  notes: joi.string().required(),
+  original_promise: joi.string().required(),
+  reviewed_by: joi.string().required(),
+  review_status: joi.string().required(),
+  submitter: joi.string().required(),
+  type: joi
+    .string()
+    .valid('promise', 'promiseUpdate')
+    .required(),
+  updated_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of update')
+});
+
+export const update = joi.object().keys({
+  assigned_tracker: joi.string().required(),
+  created_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of creation'),
+  id: joi.string().required(),
+  link: joi.string().required(),
+  notes: joi.string().required(),
+  original_promise: joi.string().required(),
+  reviewed_by: joi.string().required(),
+  review_status: joi.string().required(),
+  submitter: joi.string().required(),
+  type: joi
+    .string()
+    .valid('promise', 'promiseUpdate')
+    .required(),
+  updated_at: joi
+    .date()
+    .iso()
+    .default(utils.now, 'Time of update')
+});
