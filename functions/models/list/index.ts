03126202b7a77acc9c31dd964601c4b196fe07b5
Nazreen <nxmohamad@gmail.com>
2019-12-18 17:31:57 +0000

    instantiate db in index

--- functions/models/list/index.ts
+++ functions/models/list/index.ts
@@ -2,9 +2,12 @@ import add from './add';
 import update from './update';
 import get from './get';
 import list from './list';
+import admin from 'firebase-admin';
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true });
 
 export = () => ({
-  add,
+  add: add(db),
   update,
   get,
   list
