0f9c03d8ee5a3230fb59c58325e5bc8cd834681d
Nazreen Mohamad <nxmohamad@gmail.com>
2020-04-05 21:39:14 +0100

    update var name

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const READ = {
-  COLLECTION_NAME: process.env.COLLECTION_NAME,
-  CONCERNED_FIELDS: process.env.CONCERNED_FIELDS.split(','),
-  MATCH_KEYWORD: process.env.MATCH_KEYWORD,
-  MATCH_PROPERTY: process.env.MATCH_PROPERTY,
-  MATCH_VALUE: process.env.MATCH_VALUE
+  COLLECTION_NAME: process.env.READ_COLLECTION_NAME,
+  CONCERNED_FIELDS: process.env.READ_CONCERNED_FIELDS.split(','),
+  MATCH_KEYWORD: process.env.READ_MATCH_KEYWORD,
+  MATCH_PROPERTY: process.env.READ_MATCH_PROPERTY,
+  MATCH_VALUE: process.env.READ_MATCH_VALUE
 };
+
+const WRITE = {
+  COLLECTION_NAME: 'promises',
+  MANIFESTO_LIST_ID: 'YtIeJ0L72ged8cpKmJWx',
+  NEW_VALUE: 'Sabah'
+};
+
 export default {
-  READ
+  READ,
+  WRITE
 };
