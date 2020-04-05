0d82b5f825d8e714b9f9ce127ae02acc20c59b97
Nazreen Mohamad <nxmohamad@gmail.com>
2020-04-05 22:05:52 +0100

    set to undefined if empty string

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,5 +1,15 @@
+import utils from './utils';
 require('dotenv').config();
-const READ = {
+
+function setValuesToUndefinedIfEmptyString(object) {
+  Object.keys(object).forEach(key => {
+    const value = object[key];
+    if (value === '') object[key] = undefined;
+  });
+  return object;
+}
+
+let READ = {
   COLLECTION_NAME: process.env.READ_COLLECTION_NAME,
   CONCERNED_FIELDS: process.env.READ_CONCERNED_FIELDS.split(','),
   MATCH_KEYWORD: process.env.READ_MATCH_KEYWORD,
@@ -9,11 +19,14 @@ const READ = {
   OUTPUT_FILE: process.env.READ_OUTPUT_FILE
 };
 
-const WRITE = {
+let WRITE = {
   COLLECTION_NAME: 'promises',
   MANIFESTO_LIST_ID: 'YtIeJ0L72ged8cpKmJWx',
   NEW_VALUE: 'Sabah'
 };
+
+READ = setValuesToUndefinedIfEmptyString(READ);
+WRITE = setValuesToUndefinedIfEmptyString(WRITE);
 
 export default {
   READ,
