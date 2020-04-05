ff43c516cb6a0fc81810611dbbd261061aa07636
Nazreen <nxmohamad@gmail.com>
2020-04-05 23:11:04 +0100

    scripts: Feat filter function (#159)
    * filter config vars
    
    * create filter function

--- scripts/filter-array.ts
+++ scripts/filter-array.ts
@@ -1,5 +1,26 @@
+import config from './config';
+
+let { KEY_TO_CHECK } = config.FILTER;
+
+// warning: this does not care which duplicate it gets
+function getDuplicatesOfArray(array) {
+  let countStore = {};
+  let duplicates = [];
+  array.forEach(item => {
+    const storeKey = item[KEY_TO_CHECK];
+    if (countStore[storeKey] === undefined) {
+      countStore[storeKey] = 1;
+    } else if (countStore[storeKey] >= 1) {
+      countStore[storeKey]++;
+      duplicates.push(item);
+    }
+  });
+  return duplicates;
+}
+
 function filterArray(array) {
-  return array;
+  const result = getDuplicatesOfArray(array);
+  return result;
 }
 
 export default filterArray;
