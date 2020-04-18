d8ddf8a33912d9ad3b4a7970fb160998251874b5
Nazreen <nxmohamad@gmail.com>
2020-04-18 20:30:18 +0100

    Scripts bool parser (#163)
    * fix issues
    
    * create matchDocuments helper
    
    * rm

--- scripts/lib/batch-read.ts
+++ scripts/lib/batch-read.ts
@@ -44,11 +44,15 @@ function printLength(obj: IPrintLengthParams) {
 }
 
 async function batchRead() {
-  const allDocuments = helpers.getAllFromColection(db)(COLLECTION_NAME);
+  const allDocuments = await helpers.getAllFromColection(db)(COLLECTION_NAME);
+  // the matchObj can take on multiple sets of keys and values. currently we are using it only for one set
+  const matchObj = {
+    [MATCH_PROPERTY]: MATCH_VALUE
+  };
+  const matchedDocuments = helpers.matchDocuments(allDocuments, matchObj);
+  require('process').exit();
   // matchedDocuments option A - find by single property-value match
-  const matchedDocuments = allDocuments.filter(
-    item => item[MATCH_PROPERTY] === MATCH_VALUE
-  );
+
   // matchedDocuments option B - find by presence of keyword in selected fields
   // const matchedDocuments = allDocuments.filter(item =>
   //   findKeywordInObjectFields({
@@ -59,13 +63,14 @@ async function batchRead() {
   // );
 
   const alreadyReflecting = matchedDocuments.filter(
-    document => document.state === MATCH_KEYWORD
+    document => document[MATCH_PROPERTY] === MATCH_KEYWORD
   );
   const hasOtherValue = matchedDocuments.filter(
-    document => document.state && document.state !== MATCH_KEYWORD
+    document =>
+      document[MATCH_PROPERTY] && document[MATCH_PROPERTY] !== MATCH_KEYWORD
   );
   const valueUndefined = matchedDocuments.filter(
-    document => document.state === undefined
+    document => document[MATCH_PROPERTY] === undefined
   );
   const result = {
     alreadyReflecting,
--- scripts/lib/helpers/index.ts
+++ scripts/lib/helpers/index.ts
@@ -1,10 +1,34 @@
+import { DocumentData } from '@google-cloud/firestore';
 import utils from '../utils';
 
-const getAllFromColection = db => async collection => {
-  const snapshot = await db.collection(COLLECTION_NAME).get();
-  const allDocuments: DocumentData[] = utils.snapshotToArray(snapshot);
+const getAllFromColection = db => async collectionName => {
+  const snapshot = await db.collection(collectionName).get();
+  return utils.snapshotToArray(snapshot);
 };
 
+function parseMatchObj(matchObj) {
+  const entries = Object.entries(matchObj);
+  let result = { ...matchObj };
+  entries.forEach(entry => {
+    const [key, value] = entry;
+    if (value === 'true') result[key] = true;
+    if (value === 'false') result[key] = false;
+  });
+  return result;
+}
+
+function matchDocuments(documents, matchObj) {
+  const parsedMatchObj = parseMatchObj(matchObj);
+  const entries = Object.entries(parsedMatchObj);
+  let result = documents;
+  entries.forEach(entry => {
+    const [key, value] = entry;
+    result = documents.filter(item => item[key] === value);
+  });
+  return result;
+}
+
 export default {
-  getAllFromColection
+  getAllFromColection,
+  matchDocuments
 };
