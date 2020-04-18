5a2dc3c8eff5c9fb1bedef8931924ecca1fac689
Nazreen Mohamad <nxmohamad@gmail.com>
2020-04-18 22:25:35 +0100

    transform for MP

--- scripts/lib/batch-transform.ts
+++ scripts/lib/batch-transform.ts
@@ -2,7 +2,7 @@ import helpers from './helpers';
 
 function filterDocFunction(doc) {
   // write filtering logic here
-  return true;
+  return doc.primary_position.includes('Member of Parliament');
 }
 
 function filterDocuments(documents) {
@@ -10,7 +10,13 @@ function filterDocuments(documents) {
 }
 
 function transformDocument(doc) {
-  return doc;
+  let result = { ...doc };
+  const parliamentaryConstituency = doc.primary_position.replace(
+    'Member of Parliament for ',
+    ''
+  );
+  result.mp_for = parliamentaryConstituency;
+  return result;
 }
 
 function batchTransform(documents) {
