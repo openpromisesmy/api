2a63557340013e7ead39d1bd6bc940e5fab54038
Nazreen Mohamad <nxmohamad@gmail.com>
2020-07-06 16:42:02 +0100

    import at top

--- functions/services/db.ts
+++ functions/services/db.ts
@@ -1,10 +1,10 @@
 import admin from 'firebase-admin';
+import serviceAccount from '../secrets/google-key.json';
 // import * as functions from 'firebase-functions';
 
 const isDev = process.env.NODE_ENV === 'development';
 
 if (isDev) {
-  import serviceAccount = require('../secrets/google-key.json');
   admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 } else {
   // admin.initializeApp(functions.config().firebase);
